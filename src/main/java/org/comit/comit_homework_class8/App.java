5de23b48d654d259689b39c5a7883b0e4bd6657c
Vitek <107899056+Viterom@users.noreply.github.com>
2022-07-04 15:53:28 -0600

    first commit

---
+++ src/main/java/org/comit/comit_homework_class8/App.java
@@ -0,0 +1,93 @@
+package org.comit.comit_homework_class8;
+
+/**
+ * class 8 homework
+ *
+ */
+class App {
+    public static void main( String[] args ) {
+    
+    /* task 1
+     * {System.out.println( "Hi, I already print sentences." );}
+     */
+    
+    /* task 2
+     * int b = 273;
+     */
+    	
+    /* task 3
+     *  float f = 7.534f;
+     */
+    	
+    /* task 4
+     * Integer a = 1234;
+     *	int b = 532;
+     *	
+     *	int sum = a + b; 
+     */
+    	
+    /* task 5
+     * 	int a = 1234;
+     *	int b = 532;
+     *	
+     *	int sub = a - b;
+     */	
+    	
+    /* task 6
+     * 	int a = 1234;
+     *	int b = 532;
+     *	
+     *	int mul = a * b;
+     */	
+    	
+    /* task 7
+     *	int a = 1234;
+     *	int b = 532;
+     *	
+     *	Double div = (double)a / b;
+     */	
+    	
+    /* task 8 Write a program in Java that prints the numbers 1 to 3 on the screen.
+     * 	for(int a = 1; a < 4; a++)
+     */	
+    	
+    /* task 9 Write a program in Java that prints the numbers 1 to 9 on the screen.
+     * 	for(int a = 1; a <10; a++)
+     */	
+    	
+    /* task 10 Write a program in Java that prints on the screen the numbers from 1 to 10,000.
+     * 	for(int a = 1; a <10_001; a++)
+     */	
+    	
+    /* task 11 Write a program in Java that prints the numbers from 5 to 10 on the screen.
+     * 	for(int a = 5; a <11; a++)
+     */	
+    
+    /* task 12 Write a program in Java that prints the numbers from 5 to 15 on the screen.
+     * 	for(int a = 5; a <16; a++)
+     */		
+    
+    /* task 13 Write a program in Java that prints on the screen the numbers from 5 to 15,000.
+     * 	for(int a = 5; a <15_001; a++)
+     */		 
+    	
+    /* task 14 Write a program in Java that prints 200 times the word "hello". Note: in the source code that you write you must only enter once the word "hello".
+     *  int a = 200; 
+     *	while(a>0){
+     *	System.out.println("hello");
+     *	a--;
+     *	}
+     */	
+    	
+    /* task 15 Write a program in Java that prints the squares of the first 30 natural numbers on the screen.
+     *  for(int i = 1;i <= 30;i++) { 
+     *  System.out.println(Math.pow(i,2));}
+     */	
+    	
+    /* task 16 Write a program in Java that reads an integer from the keyboard and makes the sum of the next 100 numbers, showing the result on screen.
+     *  
+     */	
+    	
+              
+    }   
+}
